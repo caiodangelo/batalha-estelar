4e6cd55dea2f5001fd1d4c22262f9caeb89c8929
vitorbal <vitorbal@5a136461-322f-f37c-6b50-11b24dabe1af>
2010-05-03 04:58:10 +0000

    Inicio da implementacao da classe Board

---
+++ src/logica/Board.java
@@ -0,0 +1,28 @@
+package logica;
+
+import java.awt.Point;
+import java.util.Vector;
+
+/**
+ * Implements the Board class.
+ * @author Vitor / Caio
+ *
+ */
+public class Board {
+	
+	private Vector<Point> shotList;
+	private Ship[] shipList;
+	private Cell[][] grid;
+	
+	/**
+	 * Constructs and Initializes a new Board.
+	 * @param boardSize The N by N size of the new Board.
+	 * @param shipCount The number of ships allowed on the Board.
+	 */
+	Board(int boardSize, int shipCount)
+	{
+		this.shipList = new Ship[shipCount];
+		this.shotList = new Vector<Point>();
+		this.grid = new Cell[boardSize][boardSize];
+	}
+}
