dfa2f16eeb8081cf64778974beda49dd979594f1
caioluizdangelo <caioluizdangelo@5a136461-322f-f37c-6b50-11b24dabe1af>
2010-05-09 06:39:27 +0000

    funções que recebem o form dos navios feitas

--- trunk/src/logica/RecebeNavios.java
+++ trunk/src/logica/RecebeNavios.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
 
 /**
  * Servlet implementation class RecebeNavios
@@ -32,6 +33,51 @@ public class RecebeNavios extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+	}
+	
+	private Vector<Integer> getXwings(int quant, HttpServletRequest request){
+		Vector<Integer> positions = new Vector<Integer>();
+		Integer x;
+		for(int i=1; i<=quant; i++){
+			String caller = "wxing"+Integer.toString(i);
+			if(request.getParameter(caller)!=null){
+				x = Integer.parseInt(request.getParameter(caller));
+				if((x>=0)&&(x<225)){
+					positions.add(x);
+				}
+			}			
+		}
+		return positions;
+	}
+	
+	private Vector<Integer> getFalcons(int quant, HttpServletRequest request){
+		Vector<Integer> positions = new Vector<Integer>();
+		Integer x;
+		for(int i=1; i<=quant; i++){
+			String caller = "falcon"+Integer.toString(i);
+			if(request.getParameter(caller)!=null){
+				x = Integer.parseInt(request.getParameter(caller));
+				if((x>=0)&&(x<225)){
+					positions.add(x);
+				}
+			}			
+		}
+		return positions;
+	}
+	
+	private Vector<Integer> getDestroyers(int quant, HttpServletRequest request){
+		Vector<Integer> positions = new Vector<Integer>();
+		Integer x;
+		for(int i=1; i<=quant; i++){
+			String caller = "destroyer"+Integer.toString(i);
+			if(request.getParameter(caller)!=null){
+				x = Integer.parseInt(request.getParameter(caller));
+				if((x>=0)&&(x<225)){
+					positions.add(x);
+				}
+			}			
+		}
+		return positions;
 	}
 
 }
