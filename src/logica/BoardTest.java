491cb534fe65cae14574768aa860553a532b172d
vitorbal <vitorbal@5a136461-322f-f37c-6b50-11b24dabe1af>
2010-05-10 17:41:35 +0000

    correçoes e testes

--- src/logica/BoardTest.java
+++ src/logica/BoardTest.java
@@ -1,13 +1,48 @@
 package logica;
 
+import java.awt.Point;
+import java.util.Vector;
+
 import junit.framework.TestCase;
 
 public class BoardTest extends TestCase {
-	@SuppressWarnings("unused")
 	private Board board;
+	private Vector<Ship> shipList;
+	private Vector<Point> shotList;
 	
 	protected void setUp() throws Exception {
-		board = new Board(15,null);
+		shipList = new Vector<Ship>();
+		shipList.add(new Ship("teste1",1,new Point(1,1)));
+		shipList.add(new Ship("teste2",1,new Point(3,1)));
+		board = new Board(15,shipList);
+		shotList = board.getShotList();
+		shotList.add(new Point(1,1));
+		shotList.add(new Point(4,4));
+	}
+	
+	public void testBoardStateShip11() throws Exception {
+		assertEquals(true, board.getBoardState()[1][1].hasShip());
+	}
+	public void testBoardStateHit11() throws Exception {
+		assertEquals(true, board.getBoardState()[1][1].isHit());
+	}
+	public void testBoardStateShip44() throws Exception {
+		assertEquals(false, board.getBoardState()[4][4].hasShip());
+	}
+	public void testBoardStateHit44() throws Exception {
+		assertEquals(true, board.getBoardState()[4][4].isHit());
+	}
+	public void testBoardStateShip31() throws Exception {
+		assertEquals(true, board.getBoardState()[3][1].hasShip());
+	}
+	public void testBoardStateHit31() throws Exception {
+		assertEquals(false, board.getBoardState()[3][1].isHit());
+	}
+	public void testBoardStateShip55() throws Exception {
+		assertEquals(false, board.getBoardState()[5][5].isHit());
+	}
+	public void testBoardStateHit55() throws Exception {
+		assertEquals(false, board.getBoardState()[5][5].hasShip());
 	}
 
 }
---
+++ src/logica/PlayerTest.java
@@ -0,0 +1,7 @@
+package logica;
+
+import junit.framework.TestCase;
+
+public class PlayerTest extends TestCase {
+	private Player player;
+}
